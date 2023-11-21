dbbc3539ed13fcb026d8655a376e8d0a46a0edb2
Negarnbkhti42 <norob35@gmail.com>
2023-11-21 23:05:51 +0330

    Write test for shipping package

--- src/test/java/packageTest.java
+++ src/test/java/packageTest.java
@@ -1,2 +1,28 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class packageTest {
+
+    @Test
+    public void testCreatPackage() {
+        int weight = 4;
+        Package testPackage = new Package(weight);
+
+        assertEquals(weight, testPackage.getWeight());
+    }
+
+    @Test
+    public void testPackageIsShipped() {
+        int weight = 4;
+        Package testPackage = new Package(weight);
+        StandardShip standardShip = new StandardShip(testPackage);
+        testPackage.setShippingMethod(standardShip);
+
+        testPackage.sendPackage();
+
+        State packageState = testPackage.getState();
+        assertEquals(testPackage.getShippingMethod().getClass(), StandardShip.class);
+        assertEquals(packageState.getClass(), DeliveredState.class);
+    }
 }
