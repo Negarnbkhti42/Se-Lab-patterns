e7953af2d756401ba896deb9473a9c8f1f385ed6
Negarnbkhti42 <norob35@gmail.com>
2023-11-22 09:33:21 +0330

    Create main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("welcome!");
+        System.out.println("please enter package weight: ");
+        int weight = scanner.nextInt();
+
+        Package userPackage = new Package(weight);
+
+        while (true) {
+            System.out.println("enter shipping method (1 for standard and 2 for express):");
+            int answer = scanner.nextInt();
+
+            if (answer == 1) {
+                StandardShip standardShip = new StandardShip(userPackage);
+                userPackage.setShippingMethod(standardShip);
+            } else if (answer == 2) {
+                ExpressShip expressShip = new ExpressShip(userPackage);
+                userPackage.setShippingMethod(expressShip);
+            }
+
+            System.out.println(userPackage.getShippingMethod().toString());
+
+            System.out.println("should package be delivered (1 for yes and 0 for no):");
+            answer = scanner.nextInt();
+
+            if (answer == 1) {
+                userPackage.sendPackage();
+                break;
+            }
+
+            System.out.println(userPackage.getState().toString());
+        }
+
+        System.out.println(userPackage.getState().stoString());
     }
 }
